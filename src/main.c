c3687b62e619ee5ac9c5d458f757a92ef48b7c11
Piotr Apostel <117524611+LunaticDancer@users.noreply.github.com>
2026-02-08 23:05:59 +0100

    fonts, refactoring shared resources to be global

--- src/main.c
+++ src/main.c
@@ -1,38 +1,60 @@
 #include "raylib.h"
-
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+
+const int SCREEN_WIDTH = 320;
+const int SCREEN_HEIGHT = 240;
+const char* GAME_TITLE = "Yendor Delve";
+
+enum APP_STATE{
+	MainMenu,
+	Gameplay,
+};
+
+enum GAME_STATE{
+	Dungeon,
+	Battle,
+};
+
+Camera2D worldSpaceCamera = { 0 };
+Camera2D screenSpaceCamera = { 0 };
+RenderTexture2D renderTexture;
+Font titleFont;
+Font basicFont;
+Texture creatureAtlas;
+Texture itemAtlas;
+Texture generalAtlas;
+Texture dungeonAtlas;
 
 int main ()
 {
-	const int SCREEN_WIDTH = 320;
-	const int SCREEN_HEIGHT = 240;
-
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	InitWindow(GetScreenWidth(), GetScreenHeight(), "Yendor Delve");
+	InitWindow(GetScreenWidth(), GetScreenHeight(), GAME_TITLE);
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	Camera2D worldSpaceCamera = { 0 };  // Game world camera
     worldSpaceCamera.zoom = 1.0f;
-
-    Camera2D screenSpaceCamera = { 0 }; // Smoothing camera
     screenSpaceCamera.zoom = 1.0f;
 
     // Load render texture to draw all our objects
-    RenderTexture2D renderTexture = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderTexture = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
 	Rectangle sourceRec = { 0.0f, 0.0f, (float)renderTexture.texture.width, -(float)renderTexture.texture.height };
 	int displayWidth = SCREEN_WIDTH * (GetScreenHeight() / SCREEN_HEIGHT);
 	int wMargin = (GetScreenWidth() - displayWidth) / 2;
     Rectangle destRec = { wMargin, 0, displayWidth , GetScreenHeight() };
 	Vector2 origin = { 0.0f, 0.0f };
 
-	Texture creatureAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/creatures_transparent.png");
-	Texture itemAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/items_transparent.png");
-	Texture generalAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/general_transparent.png");
-	Texture dungeonAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/autotile_transparent.png");
+    titleFont = LoadFontEx("fonts/KAISG.TTF", 64, 0, 0);
+	SetTextureFilter(titleFont.texture, 0);
+    basicFont = LoadFontEx("fonts/alagard.ttf", 16, 0, 0);
+	SetTextureFilter(basicFont.texture, 0);
+
+	creatureAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/creatures_transparent.png");
+	itemAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/items_transparent.png");
+	generalAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/general_transparent.png");
+	dungeonAtlas = LoadTexture("hexanys_roguelike_tiles/Tilesheets/Transparent/autotile_transparent.png");
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
@@ -40,8 +62,9 @@ int main ()
 		BeginTextureMode(renderTexture);
             ClearBackground(BLACK);
             BeginMode2D(worldSpaceCamera);
-				DrawText("Yendor\n Delve", 64,64,32,MAROON);
-				DrawTexture(creatureAtlas, 300, 64, GREEN);
+				Vector2 textSize = MeasureTextEx(titleFont, GAME_TITLE, 64, 0);
+				Vector2 textPosition = {SCREEN_WIDTH/2 - textSize.x/2, 64};
+				DrawTextEx(titleFont, GAME_TITLE, textPosition, 64, 0, MAROON);
             EndMode2D();
         EndTextureMode();
 
